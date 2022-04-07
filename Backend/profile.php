46eea64d2bdf770998d4b6e8f079a92d5124c504
Joe Saroufim <joesaroufim.os@gmail.com>
2022-04-07 23:08:25 +0300

    profile backend: Initialized variables

---
+++ Backend/profile.php
@@ -0,0 +1,16 @@
+<?php
+
+include ("db_info.php");
+
+$name = $_POST['name'];
+$country = $_POST['country'];
+$gender = $_POST['gender'];
+$travel_country = $_POST['travel_country'];
+$college = $_POST['college'];
+$major = $_POST['major'];
+$date = $_POST['date'];
+ 
+
+
+
+?>
