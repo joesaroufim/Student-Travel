4787e05e82ef2838a6f16c6dc86afba77f2e79ec
Joe Saroufim <joesaroufim.os@gmail.com>
2022-04-07 20:57:05 +0300

    sign up backend finished

--- Backend/sign_up.php
+++ Backend/sign_up.php
@@ -7,6 +7,8 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $password = hash("sha256",$mysqli->real_escape_string($_POST['password']));
 
-
+$query = $mysqli->prepare("INSERT INTO users (name, email, password, phone_number) VALUES (?, ?, ?, ?)");
+$query->bind_param("ssss", $name, $email, $password, $phone);
+$query->execute();
 
 ?>
