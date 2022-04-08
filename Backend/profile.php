b5e2865329fc82a0e01185ba092726b42a30627f
Joe Saroufim <joesaroufim.os@gmail.com>
2022-04-08 10:48:16 +0300

    profile backend: Finished table update

--- Backend/profile.php
+++ Backend/profile.php
@@ -9,8 +9,24 @@ $travel_country = $_POST['travel_country'];
 $college = $_POST['college'];
 $major = $_POST['major'];
 $date = $_POST['date'];
- 
+$status = $_POST['status'];
+$id = $_SESSION['id'];
 
+$user_update = ("UPDATE users name = $name, gender = $gender, country = $country WHERE user_id = $id");
+
+$query = $mysqli->prepare("SELECT travel_id FROM travels WHERE  user_id = ?");
+$query->bind_param('i', $id);
+$query->execute();
+$query->store_result();
+$query->fetch();
+
+if($query->num_rows == 1){
+    $voyage_update = ("UPDATE voyages user_id = $id, destination = $travel_country, university = $college, arriving_date = $date WHERE user_id = $id");
+}else{
+    $query = $mysqli->prepare("INSERT INTO voyages (user_id, destination, university, arriving_date) VALUES (?, ?, ?, ?)");
+    $query->bind_param("ssss", $id, $travel_country, $college, $date);
+    $query->execute();
+}
 
 
 ?>
