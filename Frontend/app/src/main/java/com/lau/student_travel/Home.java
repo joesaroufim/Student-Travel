d0ba70a531b831fe4517c5b44d7c32bc02af62d1
Joe Saroufim <joesaroufim.os@gmail.com>
2022-04-12 18:35:43 +0300

    home: table added

--- Frontend/app/src/main/java/com/lau/student_travel/Home.java
+++ Frontend/app/src/main/java/com/lau/student_travel/Home.java
@@ -2,7 +2,9 @@ package com.lau.student_travel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -21,6 +23,32 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         table = (TableLayout) findViewById(R.id.table);
+
+        table.setColumnStretchable(0,true);
+        table.setColumnStretchable(1,true);
+        table.setColumnStretchable(2,true);
+
+        row = new TableRow(this);
+        col1 = new TextView(this);
+        col2= new TextView(this);
+        col3 = new TextView(this);
+        col1.setText("Joe Saroufim");
+        col1.setTextSize(20);
+        col1.setGravity(Gravity.CENTER);
+        col1.setTypeface(null, Typeface.BOLD);
+        col2.setText("McGill");
+        col2.setTextSize(20);
+        col2.setGravity(Gravity.CENTER);
+        col2.setTypeface(null, Typeface.BOLD);
+        col3.setText("hello");
+        col3.setTextSize(20);
+        col3.setGravity(Gravity.CENTER);
+        col3.setTypeface(null, Typeface.BOLD);
+        row.addView(col1);
+        row.addView(col2);
+        row.addView(col3);
+        table.addView(row);
+
 
     }
 
