b65124eb7f92f106397fdd8e70ce34cd98fbad2a
Joe Saroufim <joesaroufim.os@gmail.com>
2022-04-12 17:48:34 +0300

    home: UI of the home page

--- Frontend/app/src/main/java/com/lau/student_travel/Home.java
+++ Frontend/app/src/main/java/com/lau/student_travel/Home.java
@@ -3,13 +3,25 @@ package com.lau.student_travel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+
+    TextView col1, col2,col3;
+    TableLayout table;
+    TableRow row;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        table = (TableLayout) findViewById(R.id.table);
+
     }
 
 
