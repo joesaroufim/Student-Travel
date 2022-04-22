5a3a4e37246b2094755db550bd6770a2f327f428
Joe Saroufim <joesaroufim.os@gmail.com>
2022-04-22 14:15:12 +0300

    List: set up the UI

--- Frontend/app/src/main/java/com/lau/student_travel/MainActivity.java
+++ Frontend/app/src/main/java/com/lau/student_travel/MainActivity.java
@@ -3,15 +3,29 @@ package com.lau.student_travel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText user,pass;
     TextView signup, user_error, pass_error, cred_error;
+    public String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,4 +66,75 @@ public class MainActivity extends AppCompatActivity {
         Intent signup = new Intent (getApplicationContext(), Signup.class);
         startActivity(signup);
     }
+
+
+
+    public class PostRequest extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            //The method take String parameters and send data to the received url.
+
+            //Storing data in String objects
+            String username = params[0];
+            String password = params[1];
+            String str_url = params[2];
+
+            try {
+                // Creating a new URL connection with PHP.
+                URL url = new URL(str_url);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+
+                OutputStream out = urlConnection.getOutputStream(); //Initializing OutputStream Object.
+
+                BufferedWriter br = new BufferedWriter(new OutputStreamWriter(out, "UTF-8")); //Initializing BufferedWriter Object
+
+                // Setting the variables to be sent to the URL
+                String post_data = URLEncoder.encode("username", "UTF-8")+"="+URLEncoder.encode(username, "UTF-8")+"&"
+                        +URLEncoder.encode("password", "UTF-8")+"="+URLEncoder.encode(password, "UTF-8");
+
+                br.write(post_data); //Writing and sending data.
+                br.flush();
+                br.close();
+                out.close();
+
+                InputStream is = urlConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"));
+                message = "";
+                String line = "";
+                while((line = bufferedReader.readLine()) != null){
+                    message += line;
+                 }
+                bufferedReader.close();
+                is.close();
+                urlConnection.disconnect();
+
+                //Catching exceptions
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+    }
+
 }
--- Frontend/app/src/main/java/com/lau/student_travel/Signup.java
+++ Frontend/app/src/main/java/com/lau/student_travel/Signup.java
@@ -3,15 +3,30 @@ package com.lau.student_travel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class Signup extends AppCompatActivity {
 
     TextView req1,req2,req3,req4,req5,login;
     EditText user,number,name,pass,confirmed_pass;
+    public String message;
+    private String  post_url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +50,9 @@ public class Signup extends AppCompatActivity {
         req3.setVisibility(View.GONE);
         req4.setVisibility(View.GONE);
         req5.setVisibility(View.GONE);
+
+        post_url = "http://192.168.1.139/Mobile%20Computing/Final%20Project/Backend/sign_up.php";
+
     }
 
     public void signup (View view){
@@ -43,30 +61,121 @@ public class Signup extends AppCompatActivity {
         String phone_number = number.getText().toString();
         String password = pass.getText().toString();
         String confirmed_password = confirmed_pass.getText().toString();
-
         req5.setText("Field Required!");
+        req1.setText("Field required!");
+
+        PostRequest()
 
         if (username.isEmpty()){
             req1.setVisibility(View.VISIBLE);
             return;
         }else if (full_name.isEmpty()){
             req2.setVisibility(View.VISIBLE);
+            return;
         }else if (phone_number.isEmpty()){
             req3.setVisibility(View.VISIBLE);
+            return;
         }else if (password.isEmpty()){
             req4.setVisibility(View.VISIBLE);
+            return;
         }else if (confirmed_password.isEmpty()){
             req5.setVisibility(View.VISIBLE);
+            return;
         }else{
             if (!password.equals(confirmed_password)){
                 confirmed_pass.setText("");
-                req5.setText("Incompatible value");
+                req5.setText("Incompatible values");
                 req5.setVisibility(View.VISIBLE);
+                return;
             }else{
-                Intent login = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(login);
+                if(message.equals("correct")){
+                    Intent login = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(login);
+                }else{
+                    pass.setText("");
+                    confirmed_pass.setText("");
+                    req1.setText("Account already exists!");
+                    req1.setVisibility(View.VISIBLE);
+                    req2.setVisibility(View.GONE);
+                    req3.setVisibility(View.GONE);
+                    req4.setVisibility(View.GONE);
+                    req5.setVisibility(View.GONE);
+                }
+
             }
 
+        }
+    }
+
+    public class PostRequest extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            //The method take String parameters and send data to the received url.
+
+            //Storing data in String objects
+            String username = params[0];
+            String password = params[1];
+            String phone = params[2];
+            String full_name = params[3];
+            String str_url = params[4];
+
+            try {
+                // Creating a new URL connection with PHP.
+                URL url = new URL(str_url);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+
+                OutputStream out = urlConnection.getOutputStream(); //Initializing OutputStream Object.
+
+                BufferedWriter br = new BufferedWriter(new OutputStreamWriter(out, "UTF-8")); //Initializing BufferedWriter Object
+
+                // Setting the variables to be sent to the URL
+                String post_data = URLEncoder.encode("username", "UTF-8")+"="+URLEncoder.encode(username, "UTF-8")+"&"
+                        +URLEncoder.encode("password", "UTF-8")+"="+URLEncoder.encode(password, "UTF-8")+"&"
+                        +URLEncoder.encode("full_name", "UTF-8")+"="+URLEncoder.encode(full_name, "UTF-8")+"&"
+                        +URLEncoder.encode("phone", "UTF-8")+"="+URLEncoder.encode(phone, "UTF-8");
+
+                br.write(post_data); //Writing and sending data.
+                br.flush();
+                br.close();
+                out.close();
+
+                InputStream is = urlConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"));
+                message = "";
+                String line = "";
+                while((line = bufferedReader.readLine()) != null){
+                    message += line;
+                }
+                bufferedReader.close();
+                is.close();
+                urlConnection.disconnect();
+
+                //Catching exceptions
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
         }
     }
 
