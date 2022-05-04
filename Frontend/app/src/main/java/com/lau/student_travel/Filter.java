0f591b8c568f240288b25e7d6e5939692f869237
Joe Saroufim <joesaroufim.os@gmail.com>
2022-05-04 12:18:14 +0300

    Filter: Comments

--- Frontend/app/src/main/java/com/lau/student_travel/Filter.java
+++ Frontend/app/src/main/java/com/lau/student_travel/Filter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 public class Filter extends AppCompatActivity {
 
+    // Declaring variables
     EditText uni, location, field;
     TextView need_help, can_help;
     String college, country, status, major;
@@ -20,34 +21,41 @@ public class Filter extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
 
+        // Initializing EditText views
         uni = (EditText) findViewById(R.id.university_name);
         location = (EditText) findViewById(R.id.country_name);
         field = (EditText) findViewById(R.id.major_name);
+
+        // Initializing TextViews
         need_help = (TextView) findViewById(R.id.need_help1);
         can_help = (TextView) findViewById(R.id.can_help1);
 
     }
 
     public void canHelp(View view){
+        // Choosing "can help" as status to search for
         status = "can help";
         view.setBackgroundColor(Color.parseColor("#138B9A"));
         need_help.setBackgroundColor(Color.parseColor("#F2F6F6"));
     }
 
     public void needHelp(View view){
+        // Choosing "need help" as status to search for
         status = "need help";
         view.setBackgroundColor(Color.parseColor("#138B9A"));
         can_help.setBackgroundColor(Color.parseColor("#F2F6F6"));
     }
 
     public void search(View view){
+        // Storing the entered values in String objects
         college = uni.getText().toString();
         country = location.getText().toString();
         major = field.getText().toString();
 
+        // Checking if every field is filled
         if (status.isEmpty() || country.isEmpty() || major.isEmpty()){
             // write hereee
-        }else{
+        }else{ // Moving to the List page to list the available users by sending them their characteristics using Intent
             Intent list = new Intent(getApplicationContext(), List.class);
             list.putExtra("college", college);
             list.putExtra("country", country);
